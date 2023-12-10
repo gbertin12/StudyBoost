4543debf7755a8b2d28b0e962529c6a217bd6266
gbertin12 <gaelbertin12@gmail.com>
2023-12-10 16:13:53 +0100

    setup architecture files

---
+++ lib/data/themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const PrimaryColor = Color(0xFF6200EE);
+const PrimaryLightColor = Color(0xFF9E40C8);
+
+const SecondaryColor = Color(0xFF03DAC5);
+const SecondaryLightColor = Color(0xFF03DAC5);
+
+const BackgroundColor = Color(0xFFE5E5E5);
+
+
