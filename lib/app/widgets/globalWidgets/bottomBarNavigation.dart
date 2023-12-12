980904ef6efe8163f863a4f1015f0f8887e4b288
gbertin12 <gaelbertin12@gmail.com>
2023-12-12 07:32:27 +0100

    refactoring bottomBarNavigation

--- lib/app/widgets/globalWidgets/bottomBarNavigation.dart
+++ lib/app/widgets/globalWidgets/bottomBarNavigation.dart
@@ -3,58 +3,51 @@ import 'package:studyboost/settings/themes.dart';
 import 'package:studyboost/app/view/calendarView.dart';
 import 'package:studyboost/app/view/HomeView.dart';
 
-class CustomBottomNavigationBar extends StatelessWidget{
-  
-  final int currentIndex;
+class CustomBottomNavigationBar extends StatelessWidget {
+  	final int currentIndex;
 
-  CustomBottomNavigationBar({required this.currentIndex});
+  	CustomBottomNavigationBar({required this.currentIndex});
 
-  @override
-  Widget build(BuildContext context) {
+  	@override
+  	Widget build(BuildContext context) {
+		return BottomNavigationBar(
+	  	type: BottomNavigationBarType.fixed,
+	  	items: _buildBottomNavigationBarItems(),
+	  	currentIndex: currentIndex,
+	  	selectedItemColor: Colors.amber[800],
+	  	onTap: (int index) => _onTabTapped(context, index),
+		);
+  	}
 
-    return  BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Accueil',
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.calendar_today),
-                    label: 'Calendrier',
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.add),
-                    label: 'Ajouter',
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.view_agenda_rounded),
-                    label: 'Agenda',
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.bookmark_add),
-                    label: 'Outils',
-                ),
-                
-            ],
-            currentIndex: currentIndex,
-            selectedItemColor: Colors.amber[800],
-            onTap: (int index) => {
-                print(index),
-                if (index == 0 && currentIndex != 0) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeView()),
-                    )
-                }
-                else if (index == 1 && currentIndex != 1) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CalendarView()),
-                    )
-                }
-                
-            },
-        );
-    }
+  	List<BottomNavigationBarItem> _buildBottomNavigationBarItems() {
+		return [
+		  	_buildNavBarItem(Icons.home, 'Accueil'),
+		  	_buildNavBarItem(Icons.calendar_today, 'Calendrier'),
+		  	_buildNavBarItem(Icons.add, 'Ajouter'),
+		  	_buildNavBarItem(Icons.view_agenda_rounded, 'Agenda'),
+		  	_buildNavBarItem(Icons.bookmark_add, 'Outils'),
+		];
+  }
+
+  BottomNavigationBarItem _buildNavBarItem(IconData icon, String label) {
+	return BottomNavigationBarItem(
+	  	icon: Icon(icon),
+	  	label: label,
+	);
+  }
+
+	// Redirect to the right page when the user tap on the bottom navigation bar
+  	void _onTabTapped(BuildContext context, int index) {
+		print(index);
+		if (index != currentIndex) {
+			List<Widget> views = [
+				HomeView(), 
+				CalendarView()
+			];
+			Navigator.push(
+				context,
+				MaterialPageRoute(builder: (context) => views[index]),
+			);
+		}
+	}
 }
